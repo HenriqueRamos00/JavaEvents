1fb81b7b0b28984478b0b5a9813916e4a87a369d
Henrique <henriquekkrrr@gmail.com>
2025-10-04 22:28:51 -0300

    cors

--- src/main/java/br/com/nlw/events/controller/EventController.java
+++ src/main/java/br/com/nlw/events/controller/EventController.java
@@ -10,12 +10,14 @@ import br.com.nlw.events.model.Event;
 import br.com.nlw.events.service.EventService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 
 @RestController
+@CrossOrigin(origins = "https://java-web2.tail041186.ts.net")
 public class EventController {
 
     @Autowired
